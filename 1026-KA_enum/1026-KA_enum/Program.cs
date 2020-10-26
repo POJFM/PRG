821a0d33c329e3b011c81668a89d1888d39041be
TassiloBalbo <contact@filipsikora.com>
2020-10-26 19:02:17 +0100

    Add files via upload

---
+++ 1026-KA_enum/1026-KA_enum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _1026_KA_enum.Uchazeci;
+
+namespace _1026_KA_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Uchazeci> uchazeci = new List<Uchazeci>();
+            uchazeci.Add(new Uchazeci("Petr", "Novák", 26, gender.muz, colorHair.hneda, colorEyes.hneda));
+            uchazeci.Add(new Uchazeci("Adam", "Rychlý", 25, gender.muz, colorHair.blond, colorEyes.modra));
+            uchazeci.Add(new Uchazeci("Markéta", "Cibulová", 19, gender.zena, colorHair.cerna, colorEyes.zelena));
+            uchazeci.Add(new Uchazeci("Veronika", "Zátopová", 18, gender.zena, colorHair.hneda, colorEyes.modra));
+            uchazeci.Add(new Uchazeci("Nikol", "Zárubová", 21, gender.zena, colorHair.blond, colorEyes.modra));
+            uchazeci.Add(new Uchazeci("Bronislava", "Perníčková", 24, gender.zena, colorHair.hneda, colorEyes.modrozelena));
+            uchazeci.Add(new Uchazeci("Jakub", "Novák", 18, gender.muz, colorHair.rezava, colorEyes.seda));
+            uchazeci.Add(new Uchazeci("Petr", "Novák", 26, gender.muz, colorHair.hneda, colorEyes.hneda));
+            uchazeci.Add(new Uchazeci("Tomáš", "Zelený", 21, gender.muz, colorHair.cerna, colorEyes.hneda));
+            uchazeci.Add(new Uchazeci("Vojtěch", "Rampa", 19, gender.muz, colorHair.nezadano, colorEyes.modroseda));
+
+            foreach(var i in uchazeci)
+            {
+                i.Print();
+            }
+
+            Console.WriteLine("========================================================");
+
+            /*List<int> duplikaty = new List<int>();
+            foreach(var i in uchazeci)
+            {
+                for(int j = uchazeci.IndexOf(i) + 1; j < uchazeci.Count; j++)
+                {
+                    if(i.Equals(j))
+                    {
+                        duplikaty.Add(uchazeci.IndexOf(i));
+                    }
+                }
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1026-KA_enum/1026-KA_enum/Uchazeci.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1026_KA_enum
+{
+    class Uchazeci
+    {
+        public enum gender
+        {
+            muz,
+            zena
+        };
+        public enum colorHair
+        {
+            nezadano,
+            blond,
+            hneda,
+            cerna,
+            rezava
+        };
+        [Flags]
+        public enum colorEyes
+        {
+            nezadano,
+            modra,
+            zelena,
+            hneda,
+            seda,
+            modrozelena = modra | zelena,
+            modroseda = modra | seda
+        };
+
+        private string jmeno;
+        private string prijmeni;
+        private int vek;
+        private gender pohlavi;
+        private colorHair barvaVlasu;
+        private colorEyes barvaOci;
+
+        public string Jmeno { get => jmeno; set => jmeno = value; }
+        public string Prijmeni { get => prijmeni; set => prijmeni = value; }
+        public int Vek { get => vek; set => vek = value; }
+        public gender Pohlavi { get => pohlavi; set => pohlavi = value; }
+        public colorHair BarvaVlasu { get => barvaVlasu; set => barvaVlasu = value; }
+        public colorEyes BarvaOci { get => barvaOci; set => barvaOci = value; }
+
+        public Uchazeci(string jmeno, string prijmeni, int vek, gender pohlavi, colorHair barvaVlasu, colorEyes barvaOci)
+        {
+            this.jmeno = jmeno;
+            this.prijmeni = prijmeni;
+            this.vek = vek;
+            this.pohlavi = pohlavi;
+            this.barvaVlasu = barvaVlasu;
+            this.barvaOci = barvaOci;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Uchazeci)
+            {
+                Uchazeci details = (Uchazeci)obj;
+                return details.jmeno == this.jmeno && details.prijmeni == this.prijmeni;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Jméno: " + this.jmeno);
+            Console.WriteLine("Příjmení: " + this.prijmeni);
+            Console.WriteLine("Věk: " + this.vek);
+            Console.WriteLine("Pohlaví: " + this.pohlavi);
+            Console.WriteLine("Barva vlasů: " + this.barvaVlasu);
+            Console.WriteLine("Barva očí: " + this.barvaOci);
+            Console.WriteLine("--------------------------------------------");
+        }
+    }
+}
